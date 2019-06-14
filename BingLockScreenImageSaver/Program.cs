16cb9ce87837561186ebb065cb432333102efa6d
Dmitriy <dmitry.shebetovsky@gmail.com>
2019-06-14 20:52:53 +0300

    Add project files.

---
+++ BingLockScreenImageSaver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingLockScreenImageSaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
